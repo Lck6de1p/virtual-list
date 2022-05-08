8b6dbcee831b404625a35f9cf13d745d59b1617b
linchuanke <310504719@qq.com>
2022-05-08 19:46:08 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import VirtualList from "./components/VirtualList.vue";
+import { ref } from "vue";
+const arr = new Array(1000).fill().map((v, index) => {
+  return {
+    id: index,
+  };
+});
+const list = ref(arr);
+</script>
+
+<template>
+  <virtual-list :list-data="list">
+    <template #default="{ listItem }">
+      <div style="height: 50px">{{ listItem.id }}</div>
+    </template>
+  </virtual-list>
+</template>
+
+<style>
+</style>
